c1881ded5947a37c5f4e83f7ff33684542b73f7a
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-08-04 16:08:28 -0600

    Add files via upload

---
+++ ubicaciones_iztacalco_polanco_copia.js
@@ -0,0 +1,9 @@
+var puntos = {
+"type": "FeatureCollection",
+"name": "ubicaciones_iztacalco_polanco",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "description": "estacion_iztacalco", "direccion": "C. Álvaro Obregón 119, Santa Anita, Iztacalco, 08300 Ciudad de México, CDMX", "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -99.1193673, 19.3996151, 0.0 ] } },
+{ "type": "Feature", "properties": { "description": "estacion_polanco", "direccion": "Avenida Presidente Masaryk 29, Chapultepec Morales, Miguel Hidalgo. (Piso 9), Ciudad de México, CDMX", "id": 2 }, "geometry": { "type": "Point", "coordinates": [ -99.18234, 19.4313768, 0.0 ] } }
+]
+}
